c33a8c34280970e198cd6fe787489bb77987c42f
vansoundz <thebeatplanet.silente@gmail.com>
2020-12-08 07:49:31 +0300

    fixed landing page phone

--- src/components/layout/Landing.tsx
+++ src/components/layout/Landing.tsx
@@ -169,10 +169,11 @@ const Landing = () => {
           </div>
           <div>
             <div>
-              Phone: <a href="tel:+254726718344">+254726718344</a>
+              Phone: <a href="tel:+254716144715">++254716144715</a>
             </div>
             <div>
-              Email: <a href="mailto:ezzyfoods@gmail.com">ezzyfoods@gmail.com</a>
+              Email:{" "}
+              <a href="mailto:ezzyfoods@gmail.com">ezzyfoods@gmail.com</a>
             </div>
           </div>
         </div>
